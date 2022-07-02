0695d0c438647fc52d83f137fcc435340a12b1ac
Potcharapol Intang <85727700+Potcharapoll@users.noreply.github.com>
2022-07-02 23:40:43 +0700

    Update install.sh

--- themes/bluriz/install.sh
+++ themes/bluriz/install.sh
@@ -1,1 +1,5 @@
+#! /bin/bash
+pacman -Syu
+pacman -S --needed dunst picom polybar scrot lxappearance rofi kitty neovim xclip htop 
 
+yay -S rofi-bluetooth-git rofi-wifi-menu-git rofi-power-menu conky-lua spicetify-cli
